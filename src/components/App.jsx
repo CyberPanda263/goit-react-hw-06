91b2773c97235d48c2eec2c88b0c947fd5ac64b9
Andrej Sokolov <99323383+CyberPanda263@users.noreply.github.com>
2024-08-03 22:13:41 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,37 +5,6 @@ import ContactForm from './ContactForm/ContactForm';
 import SearchBox from './SearchBox/SearchBox'
 function App() {
  const title = "PhoneBook";
-/*
-  const [Constacts, setContats] = useState(() => {
-    const saveFeedBack = JSON.parse(window.localStorage.getItem('Constact'));
-      if(saveFeedBack) {
-        return saveFeedBack;
-      }
-      return startContacts;
-  });
-
-  useEffect(() => {
-    window.localStorage.setItem('Constact', JSON.stringify(Constacts));
-  }, [Constacts]);
-
-  const [FilterContacts, setFilterContacts] = useState("");
-
-  const addContact = (newContact) => {
-    setContats((prevcontacts) => {
-      return [...prevcontacts, newContact];
-    })
-  }
-
-  const removeContact = (contactId) => {
-    setContats((prevcontacts) => {
-      return prevcontacts.filter((contact) => contact.id !== contactId)
-    })
-  }
-
-  const constsFiltering = Constacts.filter(contact => 
-    contact.name.toLocaleLowerCase().includes(FilterContacts.toLocaleLowerCase())
-  );
-  */
 
   return (
     <div>
